803f4c0668b46bdb1c8088fadee8afb0a389d55b
thru94 <whatsappshehan6@gmail.com>
2025-07-07 22:17:01 -0700

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -1,10 +1,41 @@
-const { cmd } = require('../command');
+//*PING-PLUGIN* 😫
+//*Credits Is Gone To Old King Danuzz (DanuXz) ✨* 
+
+const {
+    cmd,
+    commands
+} = require('../lib/command')
+const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson, jsonformat} = require('../lib/functions')
+const {
+    default: makeWASocket,
+    generateWAMessageFromContent,
+    prepareWAMessageMedia,
+    proto
+} = require('@whiskeysockets/baileys')
+
 cmd({
     pattern: "ping",
-    desc: "Replies with Pong!",
-    react: "🏓",
-    category: "fun",
+    react: "📟",
+    alias: ["speed","sonic"],
+    desc: "To Check bot's ping",
+    category: "main",
+    use: '.ping',
     filename: __filename
-}, async (conn, m, store, { reply }) => {
-    reply("Pong!");
-});
+},
+async(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+const danuzz = require("@whiskeysockets/baileys")
+var inital = new Date().getTime();
+let ping = await conn.sendMessage(from , { text: '*_Pinging to Danuzz Module..._* ❗'  } )
+var final = new Date().getTime();
+await conn.sendMessage(from, { text : '《 █▒▒▒▒▒▒▒▒▒▒▒》10%' , edit : ping.key })
+await conn.sendMessage(from, { text : '《 ████▒▒▒▒▒▒▒▒》30%' , edit : ping.key })
+await conn.sendMessage(from, { text : '《 ███████▒▒▒▒▒》50%' , edit : ping.key })
+await conn.sendMessage(from, { text : '《 ██████████▒▒》80%' , edit : ping.key })
+await conn.sendMessage(from, { text : '《 ████████████》100%' , edit : ping.key })
+return await conn.sendMessage(from, { text : '📍️ *Pong ' + (final - inital) + ' Ms* ' , edit : ping.key })
+} catch (e) {
+reply('*Error !!*')
+l(e)
+}
+})
