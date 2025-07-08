9b525a1960cce6b65ed408c479699dce4c713fd7
thru94 <whatsappshehan6@gmail.com>
2025-07-07 21:43:31 -0700

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "kvlCGA7B#W0dEyiwD2jYYLxETAptBnL4b7M2DHb_ulYCXZmVPRxE", // session id
+SESSION_ID: process.env.SESSION_ID || "gv8gyY7S#0F2Qc8-f0fmLccjJiJQi1nKgdTht7fFqeNaYs5yN-O4", // session id
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", // true,false
 ANTI_DELETE: process.env.ANTI_DELETE || "true", // true,false
 MODE: process.env.MODE || "public", // public,private,groups,inbox
